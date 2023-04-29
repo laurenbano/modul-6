84be94156027ca8eef431274a233343904014ff4
laurenbano <laurenbano19@gmail.com>
2023-04-29 11:05:07 +0700

    modul 6

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+ include "app/mahasiswa.php";
+ include "inc/mahasiswa.php";
+ include "layout/mahasiswa.php";
+
+ $app = new App\Mahasiswa;
+ $inc = new Inc\Mahasiswa;
+ $layout = new Layout\Mahasiswa;
